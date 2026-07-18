efeeff90c1d360a892cdae1689865f82d2273f92
Abdul Alim <abdulalim528260@gmail.com>
2026-07-18 20:17:33 +0600

    Add Topology Sort implementation in C++

---
+++ Topology_sorter.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define endl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define sz(x) (int)x.size()
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+
+#define dbg(args...)            \
+    do                          \
+    {                           \
+        cerr << #args << " : "; \
+        faltu(args);            \
+    } while (0)
+void faltu() { cerr << endl; }
+template <typename T, typename... hello>
+void faltu(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+
+const int mx = 100005;
+
+vector<int> adj[mx];
+int indegree[mx];
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++)
+    {
+        adj[i].clear();
+        indegree[i] = 0;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        indegree[v]++;
+    }
+
+    queue<int> q;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+
+    vector<int> topo;
+
+    while (!q.empty())
+    {
+        int u = q.front();
+        q.pop();
+
+        topo.push_back(u);
+
+        for (auto v : adj[u])
+        {
+            indegree[v]--;
+
+            if (indegree[v] == 0)
+                q.push(v);
+        }
+    }
+
+    if ((int)topo.size() != n)
+    {
+        cout << "IMPOSSIBLE" << endl;
+    }
+    else
+    {
+        for (auto node : topo)
+            cout << node << " ";
+
+        cout << "\n";
+    }
+}
+
+int main()
+{
+    optimize();
+
+    solve();
+
+    return 0;
+}
